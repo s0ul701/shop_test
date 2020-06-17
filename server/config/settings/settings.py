18e07821d6387ce269151e82ac1e35b0e60d0cab
s0ul <s0ul@protonmail.com>
2020-06-17 11:56:33 +0700

    Corrected media path storage

--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -90,8 +90,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(f'{BASE_DIR}/../', 'static')
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(f'{BASE_DIR}/../', "media")
 
 #########################################################################################
 # REST framework configs
